785821abbef17146121393ce8603b6a910def1f5
Mr. Sohan <iamsohan11@gmail.com>
2026-04-28 12:32:15 +0600

    pages

---
+++ lib/feature/main_course/pages/main_course_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainCoursePage extends StatelessWidget {
+  const MainCoursePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Main Page')));
+  }
+}
